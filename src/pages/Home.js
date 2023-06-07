7fb627046a6cd63cbf454a42ff087fc5e8e8caab
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-06-07 15:12:37 +0300

    added main part of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
+import { Box } from '@mui/material';
 import React from 'react';
+import HeroBanner from '../components/HeroBanner';
+import Exercises from '../components/Exercises';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
     return (
         <div>
-            
+            <Box>
+                <HeroBanner />
+                <SearchExercises />
+                <Exercises />
+            </Box>
         </div>
     );
 };
